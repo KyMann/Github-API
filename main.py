0fdcbf9b9bea2a0de65335b31bd49a54d28dbb92
KyMann <akavir7@gmail.com>
2018-09-04 01:08:33 -0500

    worked recursive followers and make_json into class definition, added wrapper function for it also

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import secret_token
 
 def get_followers(Github_id, max=5):
     """returns a list of followers from an id, max is max number returned"""
+    # TODO: test after Auth, check dictionary names and flow
     url = "https://api.github.com/{0}/followers?client_id={1}&client_secret={2}".format(Github_id, secret_token.Client_ID, secret_token.Client_Secret)
     page = requests.get(url)
     page_content = page.text
@@ -43,12 +44,13 @@ def get_stargazers(Github_id, repository, max=3):
     return stargazers
 
 
-class Git_profile():
-    def __init__(self, Github_id, followers):
+class GitProfile():
+    def __init__(self, Github_id, depth=3):
         self.Github_id = Github_id
-        self.followers = followers
+        self.followers = return_followers(Github_id, depth)
 
     def make_json(self):
+        """makes the object, and it's follwer objects into dictionaries, then json.dumps them"""
         json_dict = {"Github_id":self.Github_id}
         follower_list = []
         for follower in self.followers:
@@ -57,47 +59,50 @@ class Git_profile():
             except AttributeError:
                 follower_list = follower
         json_dict["followers"] = follower_list
+        return json.dumps(json_dict)
 
-class Git_repo():
+class GitRepo():
     def __init__(self, Github_id, repo, stargazers):
         self.owner = Github_id
         self.name = repo
         self.stargazers = stargazers
 
 
-def return_followers(Github_id, depth=3):
+class GitStargazers():
+    def __init__(self, Github_id, repos):
+        self.Github_id = Github_id
+        self.repos = repos
+
+
+def return_followers(Github_id, depth):
     """recursive function stacks git profile objects on followers"""
     if depth == 0:  # escape case
-        followers = Git_profile(Github_id, get_followers(Github_id))
+        followers = GitProfile(Github_id, get_followers(Github_id))
         return followers
     else:
         this_followers_followers = get_followers(Github_id)  # this namespace is confusing
         for index, follower_id in this_followers_followers:  # for each follower
             this_followers_followers[index] = return_followers(follower_id, depth-1)  # we run the function again, but not as deep
-        followers = Git_profile(Github_id, this_followers_followers)  # that way we create the pyramid of followers from the bottom up
+        followers = GitProfile(Github_id, this_followers_followers)  # that way we create the pyramid of followers from the bottom up
         return followers
-
-    # Top_profile = Git_profile(Github_id, get_followers(Github_id))
-    # adding_profile = Top_profile # we can't loose our reference to the top of the pyramid, so we'll have two
-    # while index < depth:
-    #     for index, follower in enumerate(Top_profile.followers):
-    #         Top_profile.followers[index] = get_followers(follower)
-    #     index += 1
-    # return followers_json
 
 
 def make_followers_json(Github_id):
-    
+    profile = GitProfile(Github_id, 3)
+    return profile.make_json()
+
 
 def return_repos_and_stargazers(Github_id, depth=3):
     """returns json of a github users repos and the stargazers watching, 3 levels deep"""
+    # TODO: Also make repos and stargazers recursive using objects
+    # TODO: maybe break into 2 functions that will call each other? Starting with top user as a stargazer object
+    # TODO: null case will be stargazers at a certain level
     repos_and_stargazers = {}
     top_repos = get_repositories(Github_id)
     for repo in top_repos:
         top_stargazers = get_stargazers(Github_id, repo)
         for stargazer in top_stargazers:
             second_repos = get_repositories(stargazer)
-        # TODO: reach the bottom level of the nested list and call the correct function on it
 
     repos_and_stargazers_json = make_json()
     return repos_and_stargazers_json
