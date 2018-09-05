91edca6051a77e22afbed6b789e8252f3d411c4b
KyMann <akavir7@gmail.com>
2018-09-05 01:12:09 -0500

    added more tests for basic requirements

--- README.md
+++ README.md
@@ -9,8 +9,12 @@ secret_token.py
 ```TOKEN = :YourSecureToken```    
 ---------
 
-The entry point for the requirements is the function make_followers_json, and in the future make_repos_and_stargazers_json. Both will take in a github id and return the desired json.
-They work by creating classes that have recursive functions, such as return_followers, that generate more of the same object down to a desired depth. These recursive functions in turn use the api wrappers, such as get_followers, that do the work of reaching out to github and retrieving the json.
+The entry point for the requirements is the function **make_followers_json**, and in the future make_repos_and_stargazers_json. Both will take in a github id and return the desired json.
+
+They work by creating classes that have recursive functions, such as return_followers, that generate more of the same object down to a desired depth. 
+
+These recursive functions in turn use the api wrappers, such as get_followers, that do the work of reaching out to github and retrieving the information.
+The get functions are all 
 
 TODOs:
 - Test flow through of followers retrieval
--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,4 +1,4 @@
-from main import get_followers, return_followers
+from main import get_followers, return_followers, GitProfile, make_followers_json
 
 
 def test_get_followers():
@@ -10,11 +10,24 @@ def test_get_followers():
 
 
 def test_return_followers():
+    # This test will break if KyMann gets any more followers and the test_profile is no longer the first returned. Need to alter it to make it more stable
     followers_zero_depth = return_followers("KyMann", 0)
+    test_profile = GitProfile("lauramjohnson", depth=0)
     followers_one_depth = return_followers("KyMann", 1)
-    print(followers_one_depth)
+    if test_profile.followers == followers_one_depth[0].followers and len(followers_one_depth) == 4:
+        return True
+    else:
+        return False
+
+
+def test_make_followers_json():
+    expected_json = "{}"
+    output_json = make_followers_json("KyMann")
+    if output_json == expected_json:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
-    #test_get_followers()
-    test_return_followers()
+    print(test_get_followers() and test_return_followers() and test_make_followers_json())
